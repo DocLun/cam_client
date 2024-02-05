3082bb23dc1a11a0f192accbd10277c242e319d6
DocLun <91637772+DocLun@users.noreply.github.com>
2024-02-06 02:33:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
-import cv2
+import pyaudio
 import socket
-import pickle
-import struct
 
-def send_stream(server_ip, server_port):
+# Конфигурация клиента
+server_ip = '192.168.43.242'
+server_port = 8002
+chunk_size = 1024
+audio_format = pyaudio.paInt16
+channels = 1
+sample_rate = 44100
+
+def send_audio():
+    # Создаем объект PyAudio
+    audio = pyaudio.PyAudio()
+
+    # Открываем поток записи
+    stream = audio.open(format=audio_format,
+                        channels=channels,
+                        rate=sample_rate,
+                        input=True,
+                        frames_per_buffer=chunk_size)
+
+    # Создаем сокет
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, server_port))
 
-    capture = cv2.VideoCapture(0)
+    # Устанавливаем соединение с сервером
+    client_socket.connect((server_ip, server_port))
+    print(f"Установлено соединение с сервером {server_ip}:{server_port}")
 
     try:
         while True:
-            ret, frame = capture.read()
-            frame_data = pickle.dumps(frame)
+            # Читаем аудио данные из потока записи
+            audio_data = stream.read(chunk_size)
 
-            msg_size = struct.pack(">L", len(frame_data))
-            client_socket.sendall(msg_size + frame_data)
-
-            if cv2.waitKey(1) == ord('q'):
-                break
+            # Отправляем аудио данные серверу
+            client_socket.sendall(audio_data)
 
     finally:
-        capture.release()
+        # Закрываем поток записи, сокет и объект PyAudio
+        stream.stop_stream()
+        stream.close()
         client_socket.close()
-        cv2.destroyAllWindows()
+        audio.terminate()
 
-# Запускаем клиент для отправки трансляции
-send_stream('192.168.43.242', 8000)  # Замените на IP-адрес и порт сервера
+send_audio()
 
 
